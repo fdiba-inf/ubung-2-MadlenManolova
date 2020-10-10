9b828367b4cdd783ddd9396820fcd5c3a2496286
repl.it user <replituser@example.com>
2020-10-10 18:03:51 +0000

    Pi

--- src/exercise2/PiApproximation.java
+++ src/exercise2/PiApproximation.java
@@ -10,6 +10,6 @@ public class PiApproximation {
 double a = (1.0 - 1.0/3.0 + 1.0/5.0 - 1.0/7.0 + 1.0/11.0 - 1.0/13.0 + 1.0/17.0 - 1.0/19.0 + 1.0/23.0);
 double b = (4.0*a);
 
-    System.out.println ("Pi" + b);
+    System.out.println ("Pi:" + b);
   }
 }
